c72edfdb93419a97499cb6311d9494554e81d705
Haeyeon Kim <annie9725@kyonggi.ac.kr>
2025-01-22 00:18:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.80 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,26 @@
 function solution(participant, completion) {
-    // 해시맵 생성
+    var answer = '';
+    
     const hashMap = new Map();
-
-    // 참가자 배열을 해시맵에 추가 (이름별 등장 횟수 카운트)
-    for (const person of participant) {
-        hashMap.set(person, (hashMap.get(person) || 0) + 1);
+    let value;
+    
+    for(const person of participant ){
+        hashMap.set(person,(hashMap.get(person)||0)+1);
+        //해시맵 만들기
+        // {이름:명 수}
     }
-
-    // 완주자 배열을 순회하며 등장 횟수 감소
-    for (const person of completion) {
-        hashMap.set(person, hashMap.get(person) - 1);
+    
+    for (const person of completion ){
+        hashMap.set(person,(hashMap.get(person)-1));
     }
-
-    // 해시맵에서 값이 0이 아닌 키를 반환
-    for (const [key, value] of hashMap) {
-        if (value > 0) {
-            return key;
+    
+    //value가 1인거 찾기
+    for(const person of participant ){
+        if(hashMap.get(person)===1){
+            answer = person
         }
     }
+       
 
-    return ''; // 모든 사람이 완주했다면 빈 문자열 반환
+    return answer;
 }
